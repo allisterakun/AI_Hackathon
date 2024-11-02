f66e1891e9b8f8a4f9e187bdd5af80d0f3ece34e
Allister Liu <allisterakun@gmail.com>
2024-11-01 22:37:11 -0400

    filtering and combining completed
    `Daily Weather Recording`, `Lactation`, and `Milk performance recording` cannot be filtered. They are not about animals.

--- data_cleaning.py
+++ data_cleaning.py
@@ -14,6 +14,9 @@ def load_and_filter_all_csvs_in_folder(folder: str, specie: str, breed: str) -> 
 
     for j in tqdm(range(len(all_csv_files)), desc=f"{folder}", position=1, leave=False):
         csv_path = os.path.join(folder, all_csv_files[j])
+        if not all_csv_files[j].endswith(".csv"):
+            print(f"Unexpected file {csv_path}")
+            continue
         temp_df = pd.read_csv(csv_path, low_memory=False, index_col=None)
         columns = list(temp_df.columns)
         if "codiceSpecieAIA" not in columns or "codiceRazzaAIA" not in columns:
@@ -36,6 +39,3 @@ if __name__ == "__main__":
         if os.path.isdir(dataset_folder):
             cleaned = load_and_filter_all_csvs_in_folder(dataset_folder, "C4", "11")
             cleaned.to_csv(os.path.join(OUTPUT_FOLDER, f"{dataset}.csv"), index=False)
-            i += 1
-        if i >= 10:
-            raise KeyboardInterrupt
